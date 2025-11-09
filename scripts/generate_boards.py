934c9aa82e75213fc29919df4db1a3f7b792f383
tonniewhood <woodanthony15@gmail.com>
2025-11-08 20:27:21 -0700

    Got training/validation data ready

--- scripts/generate_boards.py
+++ scripts/generate_boards.py
@@ -4,45 +4,42 @@ import os
 import random
 import sys
 
-from typing import Any, Dict, List
+from typing import Dict
 
-import cairosvg
 import chess
-import yaml
 
 from PIL import Image
-from io import BytesIO
+import shutil
 
 
-# TB_PATH = "/home/tonniewhood/School/Fall_2025/CS_5600/Final_Project/tablebases/"
 ASSET_PATH = "/home/tonniewhood/School/Fall_2025/CS_5600/Final_Project/assets/"
-DATA_DIR = "/home/tonniewhood/School/Fall_2025/CS_5600/Final_Project/boardData"
-BOARD_FILE = ASSET_PATH + "Chessboard.png"
+DATA_DIR = "/home/tonniewhood/School/Fall_2025/CS_5600/Final_Project/datasets/chess_pieces/"
+TRAIN_DIR = DATA_DIR + "train/"
+VAL_DIR = DATA_DIR + "val/"
+BOARD_FILE = ASSET_PATH + "boards/{}.png"
 SPRITE_DIR = ASSET_PATH + "sprites/"
 PIECE_IMAGES = {
     chess.WHITE: {
-        chess.PAWN: "white/pawn.svg",
-        chess.KNIGHT: "white/knight.svg",
-        chess.BISHOP: "white/bishop.svg",
-        chess.ROOK: "white/rook.svg",
-        chess.QUEEN: "white/queen.svg",
-        chess.KING: "white/king.svg",
+        chess.PAWN: "white/{}/pawn.png",
+        chess.KNIGHT: "white/{}/knight.png",
+        chess.BISHOP: "white/{}/bishop.png",
+        chess.ROOK: "white/{}/rook.png",
+        chess.QUEEN: "white/{}/queen.png",
+        chess.KING: "white/{}/king.png",
     },
     chess.BLACK: {
-        chess.PAWN: "black/pawn.svg",
-        chess.KNIGHT: "black/knight.svg",
-        chess.BISHOP: "black/bishop.svg",
-        chess.ROOK: "black/rook.svg",
-        chess.QUEEN: "black/queen.svg",
-        chess.KING: "black/king.svg",
+        chess.PAWN: "black/{}/pawn.png",
+        chess.KNIGHT: "black/{}/knight.png",
+        chess.BISHOP: "black/{}/bishop.png",
+        chess.ROOK: "black/{}/rook.png",
+        chess.QUEEN: "black/{}/queen.png",
+        chess.KING: "black/{}/king.png",
     }
 }
 
-BOARD_WIDTH = 1152
-BOARD_HEIGHT = 1152
+BOARD_WIDTH = 1200
+BOARD_HEIGHT = 1200
 SQUARE_SIZE = BOARD_WIDTH // 8
-PIECE_WIDTH = int(SQUARE_SIZE * 0.85)
-PIECE_HEIGHT = int(SQUARE_SIZE * 0.85)
 
 def valid_board(board: chess.Board) -> bool:   
     """
@@ -99,8 +96,8 @@ def generate_board_image(board_image: Image.Image, piece_images: Dict[chess.Colo
             piece_img = piece_images[piece.color][piece.piece_type]
             row = 7 - (square // 8)
             col = square % 8
-            x = col * SQUARE_SIZE + (SQUARE_SIZE - PIECE_WIDTH) // 2
-            y = row * SQUARE_SIZE + (SQUARE_SIZE - PIECE_HEIGHT) // 2
+            x = col * SQUARE_SIZE + (SQUARE_SIZE - piece_img.width) // 2
+            y = row * SQUARE_SIZE + (SQUARE_SIZE - piece_img.height) // 2
             board_state_img.paste(piece_img, (x, y), piece_img)
 
     board_state_img.save(output_path)
@@ -108,7 +105,7 @@ def generate_board_image(board_image: Image.Image, piece_images: Dict[chess.Colo
     return 0
 
 
-def single_board_generation(fen: str, output_path: str, use_invalid_fen: bool = False) -> int:
+def single_board_generation(fen: str, output_path: str, use_invalid_fen: bool = False, board_style: str = "green", piece_style: str = "neo") -> int:
     """
     Generate a single chess board image from a FEN string.
 
@@ -116,6 +113,8 @@ def single_board_generation(fen: str, output_path: str, use_invalid_fen: bool = 
         fen (str): The FEN string representing the chess position.
         output_path (str): The path to save the generated image.
         use_invalid_fen (bool): If True, use an invalid FEN for testing purposes.
+        board_style (str): The style of the chess board to use.
+        piece_style (str): The style of the chess pieces to use.
 
     Returns:
         int: Exit code (0 for success, non-zero for failure).
@@ -124,16 +123,15 @@ def single_board_generation(fen: str, output_path: str, use_invalid_fen: bool = 
         raise ValueError("FEN string and output path must be provided.")
     
     # Load images
-    board_image = Image.open(BOARD_FILE).convert("RGBA")
+    board_image = Image.open(BOARD_FILE.format(board_style)).convert("RGBA")
     piece_images = {
         chess.WHITE: {},
         chess.BLACK: {}
     }
     for color in [chess.WHITE, chess.BLACK]:
         for piece_type in chess.PIECE_TYPES:
-            piece_file = SPRITE_DIR + PIECE_IMAGES[color][piece_type]
-            png_data = cairosvg.svg2png(url=piece_file, output_width=PIECE_WIDTH, output_height=PIECE_HEIGHT)
-            piece_img = Image.open(BytesIO(png_data)).convert("RGBA")
+            piece_file = SPRITE_DIR + PIECE_IMAGES[color][piece_type].format(piece_style)
+            piece_img = Image.open(piece_file).convert("RGBA")
             piece_images[color][piece_type] = piece_img
 
     # Generate the board image
@@ -145,7 +143,7 @@ def single_board_generation(fen: str, output_path: str, use_invalid_fen: bool = 
     
     return 0
 
-def multi_board_generation(fen_list_file: str, output_dir: str, use_invalid_fen: bool = False) -> int:
+def multi_board_generation(fen_list_file: str, output_dir: str, use_invalid_fen: bool = False, board_style: str = "green", piece_style: str = "neo") -> int:
     """
     Generate multiple chess board images from a list of FEN strings.
 
@@ -162,16 +160,15 @@ def multi_board_generation(fen_list_file: str, output_dir: str, use_invalid_fen:
         return 4
 
     # Load images
-    board_image = Image.open(BOARD_FILE).convert("RGBA")
+    board_image = Image.open(BOARD_FILE.format(board_style)).convert("RGBA")
     piece_images = {
         chess.WHITE: {},
         chess.BLACK: {}
     }
     for color in [chess.WHITE, chess.BLACK]:
         for piece_type in chess.PIECE_TYPES:
-            piece_file = ASSET_PATH + PIECE_IMAGES[color][piece_type]
-            png_data = cairosvg.svg2png(url=piece_file, output_width=PIECE_WIDTH, output_height=PIECE_HEIGHT)
-            piece_img = Image.open(BytesIO(png_data)).convert("RGBA")
+            piece_file = ASSET_PATH + PIECE_IMAGES[color][piece_type].format(piece_style)
+            piece_img = Image.open(piece_file).convert("RGBA")
             piece_images[color][piece_type] = piece_img
 
     with open(fen_list_file, 'r') as f:
@@ -186,176 +183,125 @@ def multi_board_generation(fen_list_file: str, output_dir: str, use_invalid_fen:
 
     return 0
 
-def generate_singletons(config_data: Dict[str, Any], board_image: Image.Image, piece_images: Dict[chess.Color, Dict[chess.Piece, Image.Image]]) -> int:
+def save_pieces_with_background(data_dir: str, sprite_style: str, square_img: Image.Image, board_style: str, is_light_square: bool) -> None:
     """
-    Generate singleton chess board images based on the configuration data.
+    Save chess piece images with a given square background for training data.
 
     Args:
-        config_data (dict): Configuration data loaded from YAML.
-
-    Returns:
-        int: Exit code (0 for success, non-zero for failure).
+        data_dir (str): Directory to save the training data.
+        sprite_style (str): Style of the chess pieces.
+        square_img (Image.Image): Image of the square background.
+        board_style (str): Style of the chess board.
+        is_light_square (bool): Whether the square is light or dark.
     """
-    if not config_data.get("generate_singles", False):
-        print("Skipping singleton generation as per configuration.")
-        return 0
-    
-    for piece, use in config_data.get("piece_singles", {}).items():
-        if not use:
-            continue
-
-        for color in [chess.WHITE, chess.BLACK]:
-            piece_img = piece_images[color][chess.PIECE_SYMBOLS.index(piece.lower())]
-
-            for square in chess.SQUARES:
-                row = 7 - (square // 8)
-                col = square % 8
-                x = col * SQUARE_SIZE + (SQUARE_SIZE - PIECE_WIDTH) // 2
-                y = row * SQUARE_SIZE + (SQUARE_SIZE - PIECE_HEIGHT) // 2
-                board_state_img = board_image.copy()
-                board_state_img.paste(piece_img, (x, y), piece_img)
-                output_path = f"{DATA_DIR}/singles/{piece}_{square}.png"
-                board_state_img.save(output_path)
-
-    return 0
-
-def generate_predefined(config_data: dict, board_image: Image.Image, piece_images: Dict[chess.Color, Dict[chess.Piece, Image.Image]]) -> int:
-    """
-    Generate predefined chess board images based on the configuration data.
-
-    Args:
-        config_data (dict): Configuration data loaded from YAML.
-        board_image (Image.Image): The base image of the chess board.
-        piece_images (Dict[chess.Color, Dict[chess.Piece, Image.Image]]): Dictionary of piece images.
-
-    Returns:
-        int: Exit code (0 for success, non-zero for failure).
-    """
-    if not config_data.get("used_fens", []):
-        print("Skipping predefined generation as per configuration.")
-        return 0
-
-    idx = 0
-    for fen in config_data["used_fens"]:
-        output_path = f"{DATA_DIR}/predefined/board_{idx+1}.png"
-        result = generate_board_image(board_image, piece_images, fen.strip(), output_path, True)
-        if result != 0:
-            print(f"Failed to generate board for FEN: {fen}")
-            return result + 2 # Combine error codes
-        idx += 1
-
-    return 0
-
-def generate_randoms(config_data: dict, board_image: Image.Image, piece_images: Dict[chess.Color, Dict[chess.Piece, Image.Image]]) -> int:
-    """
-    Generate random chess board images based on the configuration data.
-
-    Args:
-        config_data (dict): Configuration data loaded from YAML.
-        board_image (Image.Image): The base image of the chess board.
-        piece_images (Dict[chess.Color, Dict[chess.Piece, Image.Image]]): Dictionary of piece images.
-
-    Returns:
-        int: Exit code (0 for success, non-zero for failure).
-    """
-
-    random_positions = config_data.get("num_positions", 0)
-    used_fens = len(config_data.get("used_fens", []))
-
-    if random_positions <= used_fens:
-        print("Already generated enough predefined positions; skipping random generation.")
-        return 0
-
-    game_types = config_data.get("endgames", [])
-    if not game_types:
-        print("No endgame types specified for random generation; skipping.")
-        return 0
-    
-    board = chess.Board()
-
-    for pos_idx in range(random_positions-used_fens):
-        board.clear_board()
-        game_type = random.choice(game_types)
-        
-        position_generated = False
-        for _ in range(config_data.get("max_attempts")):
-            w_pieces, b_pieces = game_type.split("v")
-            pieces = [
-                *[chess.Piece.from_symbol(p) for p in w_pieces],
-                *[chess.Piece.from_symbol(p.lower()) for p in b_pieces]
-            ]
-            potential_squares = random.sample(chess.SQUARES, len(pieces))
-
-            for piece, square in zip(pieces, potential_squares):
-                board.set_piece_at(square, piece)
-
-            # If the configuration is valid, break out of the attempt loop
-            if valid_board(board) or not config_data.get("force_valid", True):
-                position_generated = True
-                break
-
-            # If we couldn't place all pieces, reset the board
-            board.clear_board()
-
-        if not position_generated:
-            print(f"Failed to generate a valid position for game type {game_type} after {config_data.get('max_attempts')} attempts.")
-            return 6  # Error code for failure to generate valid position
-
-        fen = board.fen()
-        output_path = f"{DATA_DIR}/random/board_{pos_idx+1}.png"
-        result = generate_board_image(board_image, piece_images, fen.strip(), output_path, not config_data.get("force_valid", True))
-        if result != 0:
-            print(f"Failed to generate random board for FEN: {fen}")
-            return result + 2  # Combine error codes
-    
-
-
-def training_data_generation(config_file: str) -> int:
-    """
-    Generate training data based on the provided configuration file.
-
-    Args:
-        config_file (str): Path to the training configuration YAML file.
-
-    Returns:
-        int: Exit code (0 for success, non-zero for failure).
-    """
-    
-    if not config_file:
-        print("Training configuration file must be specified.")
-        return 8
-    
-    if not os.path.isfile(config_file):
-        print(f"Configuration file {config_file} does not exist.")
-        return 8
-
-    if not os.path.splitext(config_file)[1] in ['.yaml', '.yml']:
-        print("Configuration file must be a YAML file.")
-        return 8
-
-    board_image = Image.open(BOARD_FILE).convert("RGBA")
-    piece_images = {
-        chess.WHITE: {},
-        chess.BLACK: {}
+    square_type = "light" if is_light_square else "dark"
+    color_identifiers = {chess.WHITE: "w", chess.BLACK: "b"}
+    piece_identifiers = {
+        chess.KING: "k",
+        chess.QUEEN: "q", 
+        chess.BISHOP: "b", 
+        chess.KNIGHT: "n", 
+        chess.PAWN: "p", 
+        chess.ROOK: "r", 
     }
-    for color in [chess.WHITE, chess.BLACK]:
-        for piece_type in chess.PIECE_TYPES:
-            piece_file = ASSET_PATH + PIECE_IMAGES[color][piece_type]
-            png_data = cairosvg.svg2png(url=piece_file, output_width=PIECE_WIDTH, output_height=PIECE_HEIGHT)
-            piece_img = Image.open(BytesIO(png_data)).convert("RGBA")
-            piece_images[color][piece_type] = piece_img
 
-    with open(config_file, 'r') as f:
-        config_data = yaml.safe_load(f)
+    for color in chess.COLORS:
+        for piece in chess.PIECE_TYPES:
 
-        if generate_singletons(config_data, board_image, piece_images):
-            return 8
-        if generate_predefined(config_data, board_image, piece_images):
-            return 8
-        if generate_randoms(config_data, board_image, piece_images):
-            return 8
+            piece_dir = os.path.join(TRAIN_DIR, color_identifiers[color] + piece_identifiers[piece])
+            if not os.path.exists(piece_dir):
+                os.mkdir(piece_dir)
 
-    return 0
+            piece_file = os.path.join(SPRITE_DIR, PIECE_IMAGES[color][piece].format(sprite_style))
+            piece_img = Image.open(piece_file).convert("RGBA")
+
+            # Create a new image with the square background
+            combined_img = square_img.copy()
+            x = (SQUARE_SIZE - piece_img.width) // 2
+            y = (SQUARE_SIZE - piece_img.height) // 2
+            combined_img.paste(piece_img, (x, y), piece_img)
+
+            # Save the combined image
+            save_path = os.path.join(piece_dir, f"{board_style}_{sprite_style}_{square_type}.png")
+            combined_img.save(save_path)
+            print(f"Saved training image: {save_path}")
+
+def training_data_generation(sprite_dir: str, board_dir: str) -> int:
+    """
+    Generate training data based on the assets directory path
+
+    Args:
+        sprite_dir (str): Path to the directory containing sprite images.
+        board_dir (str): Path to the directory containing board images.
+
+    Returns:
+        int: Exit code (0 for success, non-zero for failure).
+    """
+    
+    if not os.path.exists(sprite_dir):
+        print(f"Sprite directory '{sprite_dir}' does not exist.")
+        return 5
+    
+    if not os.path.exists(board_dir):
+        print(f"Board directory '{board_dir}' does not exist.")
+        return 6
+
+    sprite_styles = os.listdir(os.path.join(sprite_dir, "white"))
+    board_imgs = []
+    board_styles = []
+    for board_file in os.listdir(board_dir):
+        board_imgs.append(Image.open(os.path.join(board_dir, board_file)).convert("RGBA"))
+        board_styles.append(os.path.splitext(board_file)[0])
+
+    light_squares = [
+        board.crop((0, 0, SQUARE_SIZE, SQUARE_SIZE))
+        for board in board_imgs
+    ]
+    dark_squares = [
+        board.crop((SQUARE_SIZE, 0, 2 * SQUARE_SIZE, SQUARE_SIZE))
+        for board in board_imgs
+    ]
+
+    if os.path.exists(TRAIN_DIR):
+        shutil.rmtree(TRAIN_DIR)
+    os.mkdir(TRAIN_DIR)
+
+    if os.path.exists(VAL_DIR):
+        shutil.rmtree(VAL_DIR)
+    os.mkdir(VAL_DIR)
+
+    empty_dir = os.path.join(TRAIN_DIR, "empty")
+    if not os.path.exists(empty_dir):
+        os.mkdir(empty_dir)
+
+    for light, dark, board_style in zip(light_squares, dark_squares, board_styles):
+
+        light_square_path = os.path.join(empty_dir, f"{board_style}_light.png")
+        if not os.path.exists(light_square_path):
+            light.save(light_square_path)
+            print(f"Saved empty light square image: {light_square_path}")
+        
+        dark_square_path = os.path.join(empty_dir, f"{board_style}_dark.png")
+        if not os.path.exists(dark_square_path):
+            dark.save(dark_square_path)
+            print(f"Saved empty dark square image: {dark_square_path}")
+
+        for style in sprite_styles:
+            save_pieces_with_background(DATA_DIR, style, light, board_style, is_light_square=True)
+            save_pieces_with_background(DATA_DIR, style, dark, board_style, is_light_square=False)
+
+    for piece in os.listdir(TRAIN_DIR):
+        images = os.listdir(os.path.join(TRAIN_DIR, piece))
+        if not os.path.exists(os.path.join(VAL_DIR, piece)):
+            os.mkdir(os.path.join(VAL_DIR, piece))
+
+        val_sample = random.sample(images, max(1, int(0.3 * len(images)))) # 30% for validation
+
+        for img in val_sample:
+            src_path = os.path.join(TRAIN_DIR, piece, img)
+            dest_path = os.path.join(VAL_DIR, piece, img)
+            os.rename(src_path, dest_path)
+            print(f"Moved {os.path.basename(src_path)} to validation set")
 
 def main(args: argparse.Namespace) -> int:
     """
@@ -369,11 +315,11 @@ def main(args: argparse.Namespace) -> int:
     """
 
     if args.single:
-        return single_board_generation(args.single, args.output_file, args.invalid)
+        return single_board_generation(args.single, args.output_file, args.invalid, args.board_style, args.piece_style)
     elif args.from_file:
         return multi_board_generation(args.from_file, args.output_dir, args.invalid)  
     elif args.gen_train_data:
-        return training_data_generation(args.train_config)
+        return training_data_generation(args.sprite_dir, args.board_dir)
     else:
         print("No valid operation specified. Use --single, --from-file, or --gen-train-data.")
 
@@ -387,6 +333,9 @@ if __name__ == "__main__":
     parser.add_argument("--output-dir", "-d", type=str, help="Output directory for multiple generated images from a file.")
     parser.add_argument("--invalid", action="store_true", help="Allow generation from invalid FEN strings.")
     parser.add_argument("--gen-train-data", "-g", action="store_true", help="Generate training data based on the train_config.yaml file (found under config/).")
-    parser.add_argument("--train-config", "-c", type=str, default="config/train_config.yaml", help="Path to the training configuration YAML file.")
+    parser.add_argument("--sprite-dir", type=str, default="assets/sprites/", help="Path to the directory containing sprite images.")
+    parser.add_argument("--board-dir", type=str, default="assets/boards/", help="Path to the directory containing board images.")
     parser.add_argument("--img-size", "-i", type=int, default=BOARD_WIDTH, help="Size of the generated images (width and height in pixels).")
+    parser.add_argument("--board-style", "-b", type=str, default="green", help="Style of the chess board to use.")
+    parser.add_argument("--piece-style", "-p", type=str, default="neo", help="Style of the chess pieces to use.")
     sys.exit(main(parser.parse_args()))
---
+++ scripts/get_assets.py
@@ -0,0 +1,87 @@
+
+import argparse
+import os
+import requests
+
+
+CHESS_THEME_URL = "https://www.chess.com/chess-themes/"
+PIECE_BASE_URL = CHESS_THEME_URL + "pieces/{}/{}/{}{}.png"
+BOARD_BASE_URL = "https://images.chesscomfiles.com/chess-themes/boards/{}/150.png"
+
+def get_pieces(set_name: str, save_dir: str, piece_size: int) -> None:
+    """
+    Download chess piece images from Chess.com and save them to the specified directory.
+
+    Args:
+        set_name (str): The name of the chess piece set to download.
+        save_dir (str): The directory where the images will be saved.
+        piece_size (int): The size of the chess piece images to download. (Will be square)
+    """
+
+    if not os.path.exists(save_dir):
+        option = input(f"Directory '{save_dir}' does not exist. Create it? [y/n]: ")
+        if option.lower().startswith('y'):
+            os.makedirs(save_dir)
+        else:
+            print("Aborting download.")
+            return
+        
+    colors = ['white', 'black']
+    pieces = {'king': 'k', 'queen': 'q', 'rook': 'r', 'bishop': 'b', 'knight': 'n', 'pawn': 'p'}
+
+    for color in colors:
+        for piece in pieces:
+            piece_url = PIECE_BASE_URL.format(set_name, piece_size, color[0].lower(), pieces[piece])
+            response = requests.get(piece_url)
+            if response.status_code == 200:
+                file_dir = os.path.join(save_dir, color, set_name)
+                os.makedirs(file_dir, exist_ok=True)
+                file_path = os.path.join(file_dir, f"{piece}.png")
+                with open(file_path, 'wb') as f:
+                    f.write(response.content)
+                print(f"Downloaded {file_path}")
+            else:
+                print(f"Failed to download {piece_url}: Status code {response.status_code}")
+
+def get_boards(set_name: str, save_dir: str) -> None:
+    """
+    Download chess board images from Chess.com and save them to the specified directory.
+
+    Args:
+        set_name (str): The name of the chess board set to download.
+        save_dir (str): The directory where the images will be saved.
+    """
+
+    if not os.path.exists(save_dir):
+        option = input(f"Directory '{save_dir}' does not exist. Create it? [y/n]: ")
+        if option.lower().startswith('y'):
+            os.makedirs(save_dir)
+        else:
+            print("Aborting download.")
+            return
+
+    board_url = BOARD_BASE_URL.format(set_name)
+    response = requests.get(board_url)
+    if response.status_code == 200:
+        file_path = os.path.join(save_dir, f"{set_name}.png")
+        with open(file_path, 'wb') as f:
+            f.write(response.content)
+        print(f"Downloaded {file_path}")
+    else:
+        print(f"Failed to download {board_url}: Status code {response.status_code}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Download chess piece images from Chess.com.")
+    parser.add_argument("piece_sets", type=str, help="The name of the chess piece sets to download. (Comma-separated for multiple sets)")
+    parser.add_argument("board_sets", type=str, help="The name of the chess board sets to download. (Comma-separated for multiple sets)")
+    parser.add_argument("save_dir", type=str, help="The directory where the images will be saved.")
+    parser.add_argument("--piece_size", type=int, default=122, help="The size of the chess piece images to download. (Default: 64)")
+
+    args = parser.parse_args()
+    piece_sets = [s.strip() for s in args.piece_sets.split(",")]
+    board_sets = [s.strip() for s in args.board_sets.split(",")]
+    for set_name in piece_sets:
+        get_pieces(set_name, os.path.join(args.save_dir, "sprites"), args.piece_size)
+    for set_name in board_sets:
+        get_boards(set_name, os.path.join(args.save_dir, "boards"))
+
